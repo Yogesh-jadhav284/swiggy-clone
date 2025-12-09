e4b8549325245a9d2d4922547c3e21938c1494fa
YOUR_NAME <your_email@example.com>
2025-12-09 10:54:43 +0530

    episode 1

--- App.js
+++ App.js
@@ -1,33 +1,38 @@
-/**
- * 
- * <div id = "parent">
- *          
- * <div id = "child1">
- *          <h1>i'am the h1 tag</>
- *          <h1>i'am the h2 tag</>
- *          </div>
- * <div id = "child2">
- *          <h1>i'am the h1 tag</>
- *          <h1>i'am the h1 tag</>
- *          </div>
- * </div>   
-*
- */
-const parent = React.createElement
-("div",
-    {id:"parent"},
-    React.createElement("div", {id:"child1"}, [
-            React.createElement("h1", {}, "i'am the h1 tag"),
-            React.createElement("h2", {}, "i'am the h2 tag")
-        ],
-    React.createElement("div", {id:"child2"}, 
-            React.createElement("h1", {}, "i'am the h1 tag"),
-            React.createElement("h2", {}, "i'am the h2 tag")    
-        
-        
-)
-    )
-);
-        const root = ReactDOM.createRoot(document.getElementById("root"));
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-        root.render(parent)
+// react.createElement => would be object first of all, and when render this 
+// elemnt onto the DOM then it becoms a html....
+
+const elem = <span>React Element</span>
+
+// Component composition
+const title =   (
+    <h1 className="head" tabIndex="5">
+        {elem}
+        Namate React Using JSX
+    </h1>
+    
+    );
+const HeadingComponent = () => ( 
+       
+      <div id= "container">
+      <div>{title}</div>  
+       
+        <h2 className="heading">Namste React Functional Component </h2>
+    </div>
+   
+);
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    // root.render(heading);
+    // root.render(<HeadingComponent/>);
+
+    root.render(
+  <div>
+    
+    <HeadingComponent />
+  </div>
+);
+
+    // here heading is object then how does it shown on browser?
+    // becasue ReactDom will take heading as object and then converts to html
